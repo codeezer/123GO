fa6278e62e6cb8dc4e0791a82d77163c44a9751f
Ramesh Neupane <ezerames@gmail.com>
2016-09-17 13:56:05 +0545

    test.py

--- app/Http/Controllers/VehicleController.php
+++ app/Http/Controllers/VehicleController.php
@@ -2,99 +2,61 @@
 
 namespace App\Http\Controllers;
 
+use App;
+
 use Illuminate\Http\Request;
+
+use Response;
 
 use App\Http\Requests;
 
-class VehicleController extends Controller
+class vehicleController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-        echo 'main';
+    //
+
+    public function view($id){
+
+        $v_details = App\Bus::find($id);
+        #echo $v_details->v_no .' @ '. $v_details->c_location;
+
+        $new = json_encode($v_details);
+        return $new;
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-        echo 'create';
+    public function view_all(){
 
+        $v_details = App\Bus::all();
+        #echo $v_details->v_no .' @ '. $v_details->c_location;
+
+        $new = json_encode($v_details);
+        return $new;
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-        echo 'store';
+    public function test_all(){
+        
+        $array = '[{"id":1,"v_no":"Ba 3 Kha 8320","p_location":"67:89","c_location":"47:89","type":"Mini Bus","status":"Red","capacity":"35","route_no":"14","p_time":"2016-05-03 22:59:52","c_time":"2016-05-03 22:59:52"},{"id":2,"v_no":"Ba 2 Ka 8340","p_location":"23:67","c_location":"90:89","type":"Micro Bus","status":"Yellow","capacity":"25","route_no":"23","p_time":"2016-05-03 22:59:52","c_time":"2016-05-03 22:59:52"},{"id":3,"v_no":"Ba 1 Kha 5320","p_location":"90:89","c_location":"83:34","type":"Mini Bus","status":"Green","capacity":"35","route_no":"Sajha1","p_time":"2016-05-03 22:59:52","c_time":"2016-05-03 22:59:52"}]';
 
+        $temp_array = json_decode($array);
+        
+
+        return json_encode($temp_array);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
 
-        echo 'show';
+    public function test($id){
 
+        if ($id == 2){
+            $two = array("id"=>2,"v_no"=>"Ba 2 Ka 8340","p_location"=>"23:67","c_location"=>"90:89","type"=>"Micro Bus","status"=>"Yellow","capacity"=>"25");
+            echo (json_encode($two));}
+
+        if ($id == 1){
+            $one = '{"id":1,"v_no":"Ba 3 Kha 8320","p_location":"67:89","c_location":"47:89","type":"Mini Bus","status":"Red","capacity":"35"}';
+            return $one;}
+            
+        if ($id == 3){
+            $three = '{"id":3,"v_no":"Na 3 Kha 5320","p_location":"90:89","c_location":"83:34","type":"Mini Bus","status":"Green","capacity":"35"}';
+            return $three;}
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-        echo 'edit';
-
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-        echo 'update';
-
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
-        echo 'destroy';
-
-    }
+    
 }
